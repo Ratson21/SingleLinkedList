29a35f1517c22404de86cdf3877dd7640845b57f
ratsonfebrian <ratson.gulton@bankbsi.co.id>
2025-06-26 10:36:30 +0700

    add info

--- src/ImplSingleLinkedList/SingleLinkedList.java
+++ src/ImplSingleLinkedList/SingleLinkedList.java
@@ -4,17 +4,19 @@ public class SingleLinkedList {
     private Customer head = null;
     private Customer tail = null;
 
+    // add customer to queue
     public void push(String name) {
         Customer customer = new Customer(name);
-        if (head != null) {
-            head.next = customer;
-        }else{
+        if (head == null) {
             head = customer;
+        }else{
+            tail.next = customer;
         }
         tail = customer;
         System.out.println("add " + name + " to the list");
     }
 
+    //remove customer from queue
     public String pop() {
         if (head == null) {
             System.out.println("Tidak ada antrian.");
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         SingleLinkedList list = new SingleLinkedList();
 
+        //add customer to queue
         list.push("Andi");
         list.push("Bayu");
         list.push("Cinta");
 
+        //remove customer to queue
         list.pop();
         }
     }
